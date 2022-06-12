ae4a4ae6cfb3a02bbbecb12a8064af51ab9d0792
mmparadinha <matheusmparadinha@gmail.com>
2022-06-12 12:13:25 -0300

    Link de zap gerado e formatado corretamente. Extração das variáveis para o pedido esquematizada e OK em console, porém não funcionando no código JS.

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
-let prato;
+let comida;
 let bebida;
 let sobremesa;
+let comidaEscolhida;
+let bebidaEscolhida;
+let sobremesaEscolhida;
+let custoComida;
+let custoBebida;
+let custoSobremesa;
 
 function selecionarComida(elemento) {
-    prato = document.querySelector(".comidaSelecionada");
-    if (prato !== null) {
-        prato.classList.remove("comidaSelecionada");
+    comida = document.querySelector(".comidaSelecionada");
+    if (comida !== null) {
+        comida.classList.remove("comidaSelecionada");
     }
     elemento.classList.add("comidaSelecionada");
+    comidaEscolhida = comida.getElementsByTagName("h1")[0].innerHTML;
+    custoComida = Number(comida.getElementsByTagName("p")[0].innerHTML);
 }
 
 function selecionarBebida(elemento) {
@@ -16,6 +24,7 @@ function selecionarBebida(elemento) {
         bebida.classList.remove("bebidaSelecionada");
     }
     elemento.classList.add("bebidaSelecionada");
+    custoBebida = Number()
 }
 
 function selecionarSobremesa(elemento) {
@@ -24,16 +33,26 @@ function selecionarSobremesa(elemento) {
         sobremesa.classList.remove("sobremesaSelecionada");
     }
     elemento.classList.add("sobremesaSelecionada");
+    custoSobremesa = Number()
 }
 
 function finalizarPedido() {
-    prato = document.querySelector(".comidaSelecionada");
+    comida = document.querySelector(".comidaSelecionada");
     bebida = document.querySelector(".bebidaSelecionada");
     sobremesa = document.querySelector(".sobremesaSelecionada");
-    
-    if ((prato !== null) && (bebida !== null) && (sobremesa !== null)) {
+
+    if ((comida !== null) && (bebida !== null) && (sobremesa !== null)) {
         const elemento = document.querySelector(".finalizar");
         elemento.classList.add("pedido-completo");
         elemento.innerHTML = 'Fechar pedido';
+    }
+}
+
+function enviarPedido(elemento) {
+    const custoTotal = custoComida + custoBebida + custoSobremesa;
+
+    if (elemento.innerHTML === 'Fechar pedido') {
+        alert('${custoComida} e ${comidaEscolhida}')
+        window.open(`https://wa.me/5554996301243?text=Olá,+gostaria+de+fazer+o+pedido:%0a-+Prato:+${comidaEscolhida}%0a-+Bebida:+${bebidaEscolhida}%0a-+Sobremesa:+${sobremesaEscolhida}%0aTotal:+R$+${custoTotal}`)
     }
 }
